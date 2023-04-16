a5349370f9cda0e26de5879bd96607bc163378f5
Sergio Espinoza <sergiojose180601@gmail.com>
2023-04-16 04:13:48 +0000

    Practicas asincronicas 1-4

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'ORwTRsltPxVkpLoLJSiOqoGzjxOWelYRjVGCgfPnfvHwvkwKTYwACrWRqcOAQilZ';
+$settings['hash_salt'] = 'qDeJfobPutEIviYhimDhqPNfTFulXeQWTsIAxeoXvcjyOYqveGfjRzDQxvWHQkLn';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
